604efe53882bed1913e8d43a22c7f50e10cc3ac1
Jack Webb <git@jackwebb.me>
2020-09-13 06:54:39 +0100

    Add docstrings, comments, annotations

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from typing import List
+from typing import List, Iterator
 import praw
 import redis
 import logging
 import config
 import time
+from praw.models import Submission, Subreddit
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,47 +19,69 @@ reddit = praw.Reddit(client_id=config.CLIENT_ID,
 
 def main():
     logging.info(f"Logged in as {config.USERNAME}")
-    subreddit = reddit.subreddit(config.SUBREDDIT)
+    subreddit: Subreddit = reddit.subreddit(config.SUBREDDIT)
 
     for post in subreddit.stream.submissions():
         if post.created_utc < time.time() - config.PERIOD_HOURS * 60 * 60:
             # Ignore stream items older than the period
             continue
 
-        author = post.author.name
-        num_user_posts = r.rpush(author, post.id)
-        logging.info(f"NEW POST: {author} has made {num_user_posts} posts this period")
+        author: str = post.author.name
+        num_user_posts: int = r.rpush(author, post.id)
+        logging.debug(f"NEW POST: {author} has made {num_user_posts} posts")
 
         if num_user_posts == 1:
             r.expire(author, config.PERIOD_HOURS * 60 * 60)
+
         elif num_user_posts >= config.REMOVE_THRESHOLD:
-            logging.info(f"{author} exceeded REMOVE THRESHOLD ({config.REMOVE_THRESHOLD})")
-            first, extra = get_redis_posts(author)
+            logging.info(f"{author} exceeded REMOVE_THRESHOLD ({config.REMOVE_THRESHOLD})")
+            _, extra = get_redis_posts(author)
             remove_posts(extra)
+
         elif num_user_posts >= config.REPORT_THRESHOLD:
-            logging.info(f"{author} exceeded REPORT THRESHOLD ({config.REPORT_THRESHOLD})")
-            first, extra = get_redis_posts(author)
+            logging.info(f"{author} exceeded REPORT_THRESHOLD ({config.REPORT_THRESHOLD})")
+            _, extra = get_redis_posts(author)
             report_posts(extra)
-            send_modmail(author)
+            send_modmail_notif(author)
 
 
 def get_redis_posts(author: str) -> (str, str):
+    """Return user's first and extra post IDs
+
+    Retrieve the user's first and extra post IDs from Redis,
+    then return them as a tuple in the form (first, extra)
+
+    :param author: The username to get posts for
+    :return: Tuple of the first and extra post IDs
+    """
     return r.lindex(author, 0), r.lrange(author, 1, -1)
 
 
 def remove_posts(post_ids: List[str]):
+    """Removes posts by ID (as a moderator)
+
+    :param post_ids: List of post IDs to remove
+    """
     logging.info(f"Removing {len(post_ids)} posts")
-    for post in reddit.info([i if i.startswith('t3_') else f't3_{i}' for i in post_ids]):
+    posts: Iterator[Submission] = reddit.info([i if i.startswith('t3_') else f't3_{i}' for i in post_ids])
+    for post in posts:
+        logging.debug(f"Removing post {post.id} ({post.title})")
         post.mod.remove(spam=False, mod_note=config.REMOVE_REASON)
 
 
 def report_posts(post_ids: List[str]):
+    """Report posts by ID
+
+    :param post_ids: List of post IDs to report
+    """
     logging.info(f"Reporting {len(post_ids)} posts")
-    for post in reddit.info([i if i.startswith('t3_') else f't3_{i}' for i in post_ids]):
+    posts: Iterator[Submission] = reddit.info([i if i.startswith('t3_') else f't3_{i}' for i in post_ids])
+    for post in posts:
+        logging.debug(f"Reporting post {post.id} ({post.title})")
         post.report(config.REPORT_MESSAGE)
 
 
-def send_modmail(author: str):
+def send_modmail_notif(author: str):
     pass
 
 
